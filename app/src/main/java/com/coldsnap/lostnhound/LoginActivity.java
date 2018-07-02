b80b092abf167b981abca4f1610a4c7afea1e712
coldsnapcompany <coldsnapcompany@gmail.com>
2018-07-02 12:58:12 +0100

    set up firebase. log in page outlined. main activity needs to be changed from log in.

--- app/src/main/java/com/coldsnap/lostnhound/LoginActivity.java
+++ app/src/main/java/com/coldsnap/lostnhound/LoginActivity.java
@@ -3,17 +3,11 @@ package com.coldsnap.lostnhound;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-
-public class MainActivity extends AppCompatActivity {
-
-
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
-
 }
